f3ff1aa300d4ad13249e77953bd43596093d7427
thenayside <nemalisachin222@gmail.com>
2026-02-04 12:12:44 +0530

    test

---
+++ playwright.config.js
@@ -0,0 +1,25 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+const config =({
+  testDir: './tests',
+  timeout: 40*1000,
+  expect :{
+     timeout :40*1000,
+  },
+  reporter : 'html',
+  use: {
+  browserName : 'chromium',
+  headless : false
+    
+  },
+
+  
+});
+module.exports = config
+
---
+++ tests/samplecase.spec.js
@@ -0,0 +1,15 @@
+const {test, expect,} = require('@playwright/test');
+
+test('context play wright test',async ({browser})=>
+{
+   const context = await browser.newContext();
+   const page = await context.newPage();
+   await page.goto("https://epos.posbytz.dev");
+   console.log(await page.title());
+   await page.locator("input[placeholder='Your username']").fill("yaswanth");
+   await page.locator('[placeholder="Your password"]').fill("qwert@123");
+   await page.locator("#js_login").click();
+   await expect(page.locator('[id="username-error"]')).toContainText('These credentials');
+});
+
+
